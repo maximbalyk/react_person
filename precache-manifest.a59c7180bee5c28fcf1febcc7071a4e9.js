756344bf2a4d8f3e12de3c40ef7363c206674ab7
maximbalyk <maximbalyk@gmail.com>
2021-08-31 18:11:32 +0300

    Updates

---
+++ precache-manifest.a59c7180bee5c28fcf1febcc7071a4e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c1abfe2987a885c5d7fcf5ea2b2f79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bbb9fa5abb2d22942a39",
+    "url": "./static/css/main.68fff2fd.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb9fa5abb2d22942a39",
+    "url": "./static/js/main.7d4856c9.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
